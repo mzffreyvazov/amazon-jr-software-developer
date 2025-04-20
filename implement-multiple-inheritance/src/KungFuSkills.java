1e15a167aac4103aae90530874e3fe19a3bfb0ec
mzffreyvazov <mzffreyvazov2005@gmail.com>
2025-04-21 02:54:01 +0400

    multiple-inheritance lab

---
+++ implement-multiple-inheritance/src/KungFuSkills.java
@@ -0,0 +1,5 @@
+public interface KungFuSkills {
+    public void kungFuProtection(); // method to protect the owner from any danger
+    // public void kungFuAttack(); // method to attack the enemy
+    // public void kungFuDefend(); // method to defend against the enemy attack
+}
